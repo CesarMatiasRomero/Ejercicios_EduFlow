651f6c7b40ece10a8e38ce1954e080ce02f6b2d6
CesarMatiasRomero <cesarmatiromero@gmail.com>
2022-06-13 18:58:24 -0300

    Se agregaron imagenes y estilos. Falta LocalStorage

--- ES3/app.js
+++ ES3/app.js
@@ -9,24 +9,50 @@
 
 
 
-
+//Variables
 const inputNum = document.querySelector("#inputNum")
 const title = document.querySelector("#title")
 const precio = document.querySelector("#precio")
+const ingredientes = document.querySelector("#ingredientes")
 const buscar = document.querySelector("#buscar")
+const imagen = document.querySelector("#imagen")
 
 
 
 
-function filtrarPizzas() {
+
+//Funcion
+function filtrarPizzas(e) {
+
+ 
+
+    e.preventDefault()
     let data = inputNum.value
 
     pizzas.map(pizza => {
         if (data == pizza.Id) {
-            title.textContent += pizza.Nombre
-            precio.textContent += pizza.Precio
+            title.textContent = pizza.Nombre
+            precio.textContent = pizza.Precio
+            ingredientes.textContent = pizza.Ingredientes
+            imagen.setAttribute("src", pizza.Img)
+            inputNum.value = null
         }
     })
+
+
+   //Validaciones
+   if(data.length == ""){
+    alert("Debe ingresar un número")
+    }
+    if(inputNum.value <= 0 || inputNum.value > 10){
+        return alert("Número incorrecto, Solo se pueden utilizar numeros del 1 al 10")
+    }
+    
 }
 
+
+//Eventos
 buscar.addEventListener("click", filtrarPizzas)
+
+
+
--- ES3/db.js
+++ ES3/db.js
@@ -1,38 +1,83 @@
+const images = ["./Assets/pizza-muzarella.png", "./Assets/Pizza-Jamon.png", "./Assets/pizza-rucula.png"]
+
 const pizzas = [
   {
     Id: 1,
-    Nombre: "muzzarella",
-    Ingredientes: "Queso muzarella, aceitunas",
-    Precio: 550,
+    Nombre: "Pizza: Muzzarella",
+    Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas",
+    Precio: "Precio: $550", 
+    Img: "Assets/pizza-muzarella.png"
   },
   {
     Id: 2,
-    Nombre: "especial",
-    Ingredientes: "Queso muzarella, aceitunas, jamón, morrones",
-    Precio: 600,
+    Nombre: "Pizza: Jamón",
+    Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas, jamón, morrones",
+    Precio: "Precio: $600",
+    Img: "Assets/Pizza-Jamon.png"
   },
   {
     Id: 3,
-    Nombre: "rúcula",
-    Ingredientes: "Queso muzarella, aceitunas negras, rúcula",
-    Precio: 750,
+    Nombre: "Pizza: Rúcula",
+    Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas negras, rúcula",
+    Precio: "Precio: $750",
+    Img:"Assets/pizza-rucula.png"
   },
   {
     Id: 4,
-    Nombre: "vegetariana",
-    Ingredientes: "Queso muzarella, aceitunas, tomate, champiñones",
-    Precio: 800,
+    Nombre: "Pizza: Provolone",
+    Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas, tomate, champiñones",
+    Precio: "Precio: $800",
+    Img:"Assets/Pizza-Provolone.png"
   },
   {
     Id: 5,
-    Nombre: "napolitana",
-    Ingredientes: "Queso muzarella, aceitunas, tomate",
-    Precio: 600,
+    Nombre: "Pizza: Napolitana",
+    Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas, tomate",
+    Precio: "Precio: $600",
+    Img:"Assets/pizza-napolitana.png"
   },
   {
     Id: 6,
-    Nombre: "pepperoni",
-    Ingredientes: "Queso muzarella, aceitunas, pepperoni",
-    Precio: 750,
+    Nombre: "Pizza: Roquefort",
+    Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas, pepperoni",
+    Precio: "Precio: $750",
+    Img:"Assets/Pizza-Roquefort-.png"
   },
+{
+  Id: 7,
+  Nombre: "Pizza: Jamón Crudo",
+  Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas, jamón crudo",
+  Precio: "Precio: $850",
+  Img:"Assets/Pizza-Jamon-Crudo.png"
+},
+{
+  Id: 8,
+  Nombre: "Pizza: Calabresa",
+  Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas, longaniza",
+  Precio: "Precio: $850",
+  Img:"Assets/pizza-calabresa.png"
+  
+},
+{
+  Id: 9,
+  Nombre: "Pizza: Fugazzeta",
+  Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas, jamón crudo",
+  Precio: "Precio: $800",
+  Img:"Assets/pizza-fugazzeta.png"
+
+},
+{
+  Id: 10,
+  Nombre: "Pizza: Jamón y Palmitos",
+  Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas, jamón crudo",
+  Precio: "Precio: $950",
+  Img:"Assets/Pizza-Jamon-con-Palmitos.png", 
+  
+},
 ];
+
+
+
+
+
+
