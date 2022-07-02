f4bc2b8ab83608c2b96b9127e26a2d6c47b4c6d2
CesarMatiasRomero <cesarmatiromero@gmail.com>
2022-07-02 19:38:16 -0300

    Se agrego buscado interno por nombre

--- ES3/JS/app.js
+++ ES3/JS/app.js
@@ -18,80 +18,122 @@ const buscar = document.querySelector("#buscar")
 const imagen = document.querySelector("#imagen")
 const spinner = document.querySelector(".spinner")
 const resultado = document.querySelector(".resultado")
+const formulario = document.querySelector("#formulario")
 
-guardarLocalstorage();
+
+const filtrar = (e) => {
+    // e.preventDefault()
+    console.log(inputNum.value);
+    
+
+    const texto = inputNum.value.toLowerCase()
+
+    for ( let pizza of pizzas){
+        let nombre = pizza.Nombre.toLowerCase()
+        if(nombre.indexOf(texto) !== -1){
+                
+                title.textContent = `Pizza: ${pizza.Nombre}`
+                precio.textContent = pizza.Precio
+                ingredientes.textContent = pizza.Ingredientes
+                imagen.setAttribute("src", pizza.Img)
+        }
+    }
+
+    if(inputNum.value === ""){
+        alert("Debe ingresar un valor")
+    }
+
+   
+}
+
+
+
+buscar.addEventListener("click", filtrar)
+inputNum.addEventListener("keyup", filtrar)
+
+
+
+
+
+
+// guardarLocalstorage();
 
 
 
 //Funciones
 
 //Solo se permiten numeros en el input
-function soloNumeros(e){
-    var key = window.Event ? e.which : e.keyCode
-    return (key >= 48 && key <= 57)
-}
+// function soloNumeros(e){
+//     var key = window.Event ? e.which : e.keyCode
+//     return (key >= 48 && key <= 57)
+// }
 
-//Filtrado de pizzas por ID
-function filtrarPizzas(e) {
-    e.preventDefault()
-    let data = inputNum.value
+// //Filtrado de pizzas por ID
+// function filtrarPizzas(e) {
+//     e.preventDefault()
+//     let data = inputNum.value
+    
 
-    pizzas.map(pizza => {
-        if (data == pizza.Id) {
-            mostrarSpinner()
+//     pizzas.map(pizza => {
+//         if (data == pizza.Id) {
+//             console.log("mostrando pizza");
+//             mostrarSpinner()
 
-            setTimeout(() => {
-                title.textContent = pizza.Nombre
-                precio.textContent = pizza.Precio
-                ingredientes.textContent = pizza.Ingredientes
-                imagen.setAttribute("src", pizza.Img)
-                inputNum.value = null
+//             setTimeout(() => {
+//                 title.textContent = `Pizza: ${pizza.Nombre}`
+//                 precio.textContent = pizza.Precio
+//                 ingredientes.textContent = pizza.Ingredientes
+//                 imagen.setAttribute("src", pizza.Img)
+//                 inputNum.value = null
 
-            }, 3000);
+//             }, 3000);
 
             
-        }
+//         }
 
         
-    })
+//     })
 
-   //Validaciones
-   if(data.length == ""){
-    alert("Debe ingresar un número")
-    }
-    if(data <= 0 || data > 10){
-    alert("Número incorrecto, Solo se pueden utilizar numeros del 1 al 10")
-    }
+//    //Validaciones
+//    if(data.length == ""){
+//     alert("Debe ingresar un valor número")
+//     }
+//     if(data <= 0 || data > 10){
+//     alert("Número incorrecto, Solo se pueden utilizar numeros del 1 al 10")
+//     }
     
     
-}
-//Guardamos en Localstorage
-function guardarLocalstorage() {
-    localStorage.setItem("Pizzas", JSON.stringify(pizzas))
-}
+// }
+// //Guardamos en Localstorage
+// function guardarLocalstorage() {
+//     localStorage.setItem("Pizzas", JSON.stringify(pizzas))
+// }
 
 //Mostramos el spinner loader
-function mostrarSpinner(){
-    spinner.style.display = "block";
+// function mostrarSpinner(){
+//     spinner.style.display = "block";
 
-    setTimeout(() => {
-        spinner.style.display = 'none';
+//     setTimeout(() => {
+//         spinner.style.display = 'none';
         
-      }, 3000);
+//       }, 3000);
 
-}
-
-
+//  }
 
 
-//Eventos
-buscar.addEventListener("click", filtrarPizzas, soloNumeros , mostrarSpinner )
 
-inputNum.addEventListener('keypress', function (e){
-	if (!soloNumeros(e)){
-  	e.preventDefault();
-  }
-})
+
+
+
+
+// //Eventos
+// buscar.addEventListener("click", filtrarPizzas, mostrarSpinner )
+
+// inputNum.addEventListener('keypress', function (e){
+// 	if (!soloNumeros(e)){
+//   	e.preventDefault();
+//   }
+// })
 
 
 
--- ES3/JS/db.js
+++ ES3/JS/db.js
@@ -1,56 +1,56 @@
 const pizzas = [
   {
     Id: 1,
-    Nombre: "Pizza: Muzzarella",
+    Nombre: "Muzzarella",
     Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas",
     Precio: "Precio: $550", 
     Img: "Assets/pizza-muzarella.png"
   },
   {
     Id: 2,
-    Nombre: "Pizza: Jamón",
+    Nombre: "Jamón",
     Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas, jamón, morrones",
     Precio: "Precio: $600",
     Img: "Assets/Pizza-Jamon.png"
   },
   {
     Id: 3,
-    Nombre: "Pizza: Rúcula",
+    Nombre: "Rúcula",
     Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas negras, rúcula",
     Precio: "Precio: $750",
     Img:"Assets/pizza-rucula.png"
   },
   {
     Id: 4,
-    Nombre: "Pizza: Provolone",
+    Nombre: "Provolone",
     Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas, tomate, champiñones",
     Precio: "Precio: $800",
     Img:"Assets/Pizza-Provolone.png"
   },
   {
     Id: 5,
-    Nombre: "Pizza: Napolitana",
+    Nombre: "Napolitana",
     Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas, tomate",
     Precio: "Precio: $600",
     Img:"Assets/pizza-napolitana.png"
   },
   {
     Id: 6,
-    Nombre: "Pizza: Roquefort",
+    Nombre: "Roquefort",
     Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas, pepperoni",
     Precio: "Precio: $750",
     Img:"Assets/Pizza-Roquefort-.png"
   },
 {
   Id: 7,
-  Nombre: "Pizza: Jamón Crudo",
+  Nombre: "Jamón Crudo",
   Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas, jamón crudo",
   Precio: "Precio: $850",
   Img:"Assets/Pizza-Jamon-Crudo.png"
 },
 {
   Id: 8,
-  Nombre: "Pizza: Calabresa",
+  Nombre: "Calabresa",
   Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas, longaniza",
   Precio: "Precio: $850",
   Img:"Assets/pizza-calabresa.png"
@@ -58,15 +58,15 @@ const pizzas = [
 },
 {
   Id: 9,
-  Nombre: "Pizza: Fugazzeta",
-  Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas, jamón crudo",
+  Nombre: "Fugazzeta",
+  Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas, cebollas salteadas",
   Precio: "Precio: $800",
   Img:"Assets/pizza-fugazzeta.png"
 
 },
 {
   Id: 10,
-  Nombre: "Pizza: Jamón y Palmitos",
+  Nombre: "Jamón y Palmitos",
   Ingredientes: "Ingredientes: Salsa de tomate, queso muzarella, aceitunas, jamón crudo",
   Precio: "Precio: $950",
   Img:"Assets/Pizza-Jamon-con-Palmitos.png", 
